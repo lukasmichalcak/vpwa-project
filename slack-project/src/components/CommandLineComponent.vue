da8291184bcf59f8ad63eee5a4347f42890cc3b4
lukasmichalcak <xmichalcak@stuba.sk>
2024-10-20 13:15:52 +0200

    send button

--- slack-project/src/components/CommandLineComponent.vue
+++ slack-project/src/components/CommandLineComponent.vue
@@ -7,7 +7,18 @@
     label-color="white"
     input-class="custom-input-text"
     @keyup.enter="handleEnter"
-  />
+  >
+    <template v-slot:append>
+      <q-btn
+        flat
+        round
+        glossy
+        color="positive"
+        icon="send"
+        @click="handleEnter"
+      />
+    </template>
+  </q-input>
 </template>
 
 <script lang="ts">
@@ -32,8 +43,7 @@ export default defineComponent({
     handleEnter() {
       if (this.text.trim() === '/list') {
         this.$emit('toggleRightDrawer');
-      }
-      if (this.text.startsWith('/join')) {
+      } else if (this.text.startsWith('/join')) {
         const parts = this.text.split(' ');
         let isPrivate = false;
         if (parts[parts.length - 1] === '[private]') {
@@ -43,18 +53,15 @@ export default defineComponent({
         const channelName = parts.slice(1).join(' ');
         const commandJoin = { channelName, isPrivate };
         this.join({ command: commandJoin });
-      }
-      if (this.text.startsWith('/quit')) {
+      } else if (this.text.startsWith('/quit')) {
         const parts = this.text.split(' ');
         const channelName = parts[1];
         this.quit({ command: channelName });
-      }
-      if (this.text.startsWith('/cancel')) {
+      } else if (this.text.startsWith('/cancel')) {
         const parts = this.text.split(' ');
         const channelName = parts[1];
         this.cancel({ command: channelName });
-      }
-      if (this.text.trim() !== '') {
+      } else if (this.text.trim() !== '') {
         const newMessage = {
           name: 'me',
           avatar: require('@/assets/images/verifier_logo.png'),
